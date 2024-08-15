a4bf7d033055831e7b109542594606512c93a33b
authid-sdk-ci <svc.authid-sdk-ci@twilio.com>
2024-08-15 00:08:15 +0000

    Update to version v0.1.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "TwilioVerifyPasskeys",
-         url: "https://github.com/twilio/twilio-verify-passkeys-ios/releases/download/v0.1.0/TwilioVerifyPasskeys.xcframework.zip",
+         url: "https://github.com/twilio/twilio-verify-passkeys-ios/releases/download/v0.1.0/TwilioPasskeysAuthentication.xcframework.zip",
          checksum: "eae45efdd6f623de7f1f81e017e04af41d8fff330b8a75dfa4082a17f6778eee")
    ]
 )
